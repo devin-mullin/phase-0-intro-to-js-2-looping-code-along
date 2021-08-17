277796d35038e5d85df49d6b7dc1874da5afcca7
Devin Mullin <devin.mullin90@gmail.com>
2021-08-17 14:28:59 -0500

    Completed assignment!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Devin", "Emily", "Mom and Dad"];
+const birthday = []
+function writeCards(birthday) {
+    for(let i = 0; i < birthday.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful gift!`)
+        debugger
+    }
+    return writeCards
+}
+
+writeCards(birthday)
+
+let countdown = 10
+while (countdown >= 0) {
+    console.log(countdown--)
+}
+
+countdown
